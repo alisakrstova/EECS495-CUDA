33e8483741e5dbee5fe662eb8eddda6ac6888f58
Haotian Liu <liuhaotian.air@gmail.com>
2012-02-24 15:06:19 -0600

    Test again

--- matrixmul/matrixmul.cu
+++ matrixmul/matrixmul.cu
@@ -173,7 +173,12 @@ void MatrixMulOnDevice(const Matrix M, const Matrix N, Matrix P)
 	putchar('\n');
 	*/
 	CopyFromDeviceMatrix(P, D_P);
-
+	int i;
+	for(i=0;i<M.width*M.width;i++){
+		if(i%16==0)putchar('\n');
+		printf("%8f,",P.elements[i]);
+	}
+	putchar('\n');
 	cudaFree(D_M.elements);
 	cudaFree(D_N.elements);
 	cudaFree(D_P.elements);
