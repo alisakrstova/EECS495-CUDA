5ccdfea44d540b2fb19a3441a5b2e71cffc44cab
Haotian Liu <liuhaotian.air@gmail.com>
2012-03-06 12:17:19 -0600

    Add the header for functions

--- histogram/opt_2dhisto.h
+++ histogram/opt_2dhisto.h
@@ -1,9 +1,10 @@
 #ifndef OPT_KERNEL
 #define OPT_KERNEL
 
-void opt_2dhisto( /*Define your own function parameters*/ );
+void opt_2dhisto(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH]);
 
 /* Include below the function headers of any other functions that you implement */
 
+__global__ void opt_2dhistoKernel(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH]);
 
 #endif
