c611ecfca2ac9fdb11fcd871db9a9f080dea4c25
Haotian Liu <liuhaotian.air@gmail.com>
2012-03-06 13:17:07 -0600

    Privatize the fund

--- histogram/opt_2dhisto.cu
+++ histogram/opt_2dhisto.cu
@@ -6,6 +6,8 @@
 #include "util.h"
 #include "ref_2dhisto.h"
 
+__global__ void opt_2dhistoKernel(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH]);
+
 void opt_2dhisto(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH])
 {
     /* This function should only contain a call to the GPU 
--- histogram/opt_2dhisto.h
+++ histogram/opt_2dhisto.h
@@ -5,8 +5,6 @@ void opt_2dhisto(uint32_t *input[], size_t height, size_t width, uint8_t bins[HI
 
 /* Include below the function headers of any other functions that you implement */
 
-__global__ void opt_2dhistoKernel(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH]);
-
 void* AllocateDevice(size_t size);
 
 void CopyToDevice(void* D_device, void* D_host, size_t size);
