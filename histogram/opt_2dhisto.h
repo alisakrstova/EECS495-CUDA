803239d2474db37deb275288e2a76797283cba17
Haotian Liu <liuhaotian.air@gmail.com>
2012-03-06 12:19:51 -0600

    Include cuda util.h

--- histogram/opt_2dhisto.h
+++ histogram/opt_2dhisto.h
@@ -7,4 +7,6 @@ void opt_2dhisto(uint32_t *input[], size_t height, size_t width, uint8_t bins[HI
 
 __global__ void opt_2dhistoKernel(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH]);
 
+#include <cutil.h>
+
 #endif
