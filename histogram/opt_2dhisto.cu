94d0fab65ddc344c182d01bf6bfaafc2c355afff
Haotian Liu <liuhaotian.air@gmail.com>
2012-03-06 13:49:15 -0600

    Dummy test

--- histogram/opt_2dhisto.cu
+++ histogram/opt_2dhisto.cu
@@ -22,7 +22,7 @@ void opt_2dhisto(uint32_t* input, size_t height, size_t width, uint8_t* bins)
 /* Include below the implementation of any other functions you need */
 
 __global__ void opt_2dhistoKernel(uint32_t *input, size_t height, size_t width, uint8_t* bins){
-
+	if(threadIdx.x == 0){
     for (size_t j = 0; j < height; ++j)
     {
         for (size_t i = 0; i < width; ++i)
@@ -33,6 +33,7 @@ __global__ void opt_2dhistoKernel(uint32_t *input, size_t height, size_t width, 
                 ++bins[input[j * height + i]];
         }
     }
+	}
 }
 
 void* AllocateDevice(size_t size){
