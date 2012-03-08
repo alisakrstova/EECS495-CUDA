94777498441cd4ddb7fa5530fd23ea350b65fc7f
Haotian Liu <liuhaotian.air@gmail.com>
2012-03-07 19:18:29 -0600

    sync

--- histogram/opt_2dhisto.cu
+++ histogram/opt_2dhisto.cu
@@ -33,7 +33,7 @@ __global__ void opt_2dhistoKernel(uint32_t *input, size_t height, size_t width, 
 
 	//if (s_bins[input[idx]] < UINT8_MAX)
 	atomicAdd(s_bins + input[idx], 1);
-	//__syncthreads();
+	__syncthreads();
 		//++bins[input[j * height + idx]];
 	//if (s_bins[input[idx + 512]] < UINT8_MAX)
 	atomicAdd(s_bins + input[idx + 512], 1);
